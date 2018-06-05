e16c09e4ac61121d0a47d7457de2e366a9496b51
Joshua Cook <joshuacook@Joshuas-iMac.local>
2018-06-05 13:44:30 -0700

    latest

---
+++ source/05-ames/items.py
@@ -0,0 +1,27 @@
+items = [
+    {'form': 'square', 'letter': 'E', 'color': 'blue', 'border': False},
+    {'form': 'circle', 'letter': 'A', 'color': 'red', 'border': True},
+    {'form': 'circle', 'letter': 'B', 'color': 'green', 'border': False},
+    {'form': 'diamond', 'letter': 'E', 'color': 'red', 'border': True},
+    
+    {'form': 'diamond', 'letter': 'E', 'color': 'red', 'border': True},
+    {'form': 'diamond', 'letter': 'C', 'color': 'blue', 'border': False},
+    {'form': 'square', 'letter': 'A', 'color': 'green', 'border': False},
+    {'form': 'diamond', 'letter': 'D', 'color': 'green', 'border': True},
+    
+    {'form': 'star', 'letter': 'C', 'color': 'silver', 'border': True},
+    {'form': 'square', 'letter': 'C', 'color': 'silver', 'border': False},
+    {'form': 'circle', 'letter': 'B', 'color': 'green', 'border': False},
+    {'form': 'circle', 'letter': 'A', 'color': 'red', 'border': True},
+    
+    {'form': 'square', 'letter': 'A', 'color': 'green', 'border': False},
+    {'form': 'circle', 'letter': 'D', 'color': 'green', 'border': True},
+    {'form': 'star', 'letter': 'B', 'color': 'blue', 'border': True},
+    {'form': 'circle', 'letter': 'B', 'color': 'blue', 'border': True},
+    
+    {'form': 'square', 'letter': 'E', 'color': 'blue', 'border': False},
+    {'form': 'diamond', 'letter': 'E', 'color': 'red', 'border': True},
+    {'form': 'star', 'letter': 'B', 'color': 'blue', 'border': True},
+    {'form': 'square', 'letter': 'C', 'color': 'purple', 'border': False},
+    {'form': 'square', 'letter': 'C', 'color': 'silver', 'border': False},
+]
